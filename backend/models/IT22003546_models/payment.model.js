3612e6019919cc713d805c6cb29793c640966d2b
Thamindu Vimansha <159893134+Tommy14@users.noreply.github.com>
2024-10-16 13:27:15 +0530

    Refactor billing, payment, and pricing models

--- backend/models/IT22003546_models/payment.model.js
+++ backend/models/IT22003546_models/payment.model.js
@@ -5,28 +5,30 @@ const paymentSchema = new mongoose.Schema({
         type: String, 
         required: true, 
         unique: true 
-    }, // Unique identifier for the payment
+    }, // Unique identifier for the payment from FE
     customerID: { 
         type: String, 
         required: true 
-    },              // Unique identifier for the customer
+    }, // Unique identifier for the customer from parameter
     paymentDate: { 
         type: Date, 
         required: true 
-    },               // Date of the payment
+    }, // Date of the payment
     amount: { 
         type: Number, 
-        required: true
-    },                  // Amount paid by the customer
+        required: true,
+        min: 0
+    }, // Amount paid by the customer
     paymentMethod: { 
         type: String, 
         required: true 
-    },           // Method of payment
+    }, // Method of payment
     createdAt: { 
         type: Date, 
         default: Date.now 
     },
 });
 
+// Create the Payment model
 const Payment = mongoose.model('Payment', paymentSchema);
 export default Payment;
--- backend/models/IT22003546_models/pricing.model.js
+++ backend/models/IT22003546_models/pricing.model.js
@@ -3,13 +3,15 @@ import mongoose from 'mongoose';
 const pricingSchema = new mongoose.Schema({
     item: { 
         type: String, 
-        required: true 
-    },
+        required: true ,
+        unique : true
+    }, // The name of the item being priced
 
     pricePerUnit: { 
         type: Number, 
-        required: true 
-    },
+        required: true,
+        min: 0
+    }, // Price per unit for the item
 });
 
 const Pricing = mongoose.model('Pricing', pricingSchema);
